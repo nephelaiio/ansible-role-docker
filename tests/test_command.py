fa7b449b8a83ddf9cafbcc7ef741ff901a222905
Ted Cook <teodoro.cook@gmail.com>
2016-11-07 14:00:38 -0600

    Added docker-py package to default helpers

--- tests/test_command.py
+++ tests/test_command.py
@@ -2,8 +2,9 @@ from testinfra.utils.ansible_runner import AnsibleRunner
 
 
 testinfra_hosts = \
-        AnsibleRunner('.molecule/ansible_inventory').get_hosts('test')
+    AnsibleRunner('.molecule/ansible_inventory').get_hosts('test')
 
 
 def test_command(Command):
     assert Command('docker --version').rc == 0
+    assert Command('python -c "import docker"').rc == 0
