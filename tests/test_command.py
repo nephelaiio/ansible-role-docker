1295ccd8b7f6b43b7832c68ca0b39c42b942c2fc
Ted Cook <teodoro.cook@gmail.com>
2017-07-30 18:12:34 -0600

    Add docker client test

--- tests/test_command.py
+++ tests/test_command.py
@@ -5,9 +5,10 @@ testinfra_hosts = \
     AnsibleRunner('.molecule/ansible_inventory').get_hosts('test')
 
 
-def test_command(Command):
+def test_client(Command):
     assert Command('docker --version').rc == 0
     assert Command('python -c "import docker"').rc == 0
+    assert Command('docker ps').rc == 0
 
 
 def test_service(Service):
