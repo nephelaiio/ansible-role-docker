6bfcdd60768ee6ae366a6bad2d4c252c9173966b
Ted Cook <teodoro.cook@gmail.com>
2019-07-21 22:52:57 -0600

    Fix testinfra deprecation errors

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,12 +6,11 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_client(Command):
-    assert Command('docker --version').rc == 0
-    assert Command('python -c "import docker"').rc == 0
-#    assert Command('docker ps').rc == 0
+def test_client(host):
+    assert host.command('docker --version').rc == 0
+    assert host.command('python -c "import docker"').rc == 0
 
 
-def test_service(Service):
-    assert Service('docker').is_enabled
-    assert Service('docker').is_running
+def test_service(host):
+    assert host.service('docker').is_enabled
+    assert host.service('docker').is_running
