a440083c14867a71a7feb6bf1517f4852d00495b
Ted Cook <teodoro.cook@gmail.com>
2021-01-08 22:29:51 -0600

    Remove conditional for dependency bootstrap

--- install.sh
+++ install.sh
@@ -61,9 +61,7 @@ else
     cp -a . "$tmpdir"
 fi
 pushd "$tmpdir/install"
-if [ -f ../requirements.yml ]; then
-    ansible-galaxy install -r nephelaiio.docker
-fi
+ansible-galaxy install -r nephelaiio.docker
 ansible-playbook --become --connection=local -i inventory playbook.yml -e docker_pip_helpers='[]' -e docker_user="$USER"
 popd
 
